55275f871eaf69054d7bd5929253ce615a4221d7
kms0207 <kms0207@ya.ru>
2021-12-08 15:39:36 +0400

    Initial commit

---
+++ Assets/Scripts/droneMovementScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class droneMovementScript : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    private Vector2 movementForce = new Vector2 (0, 0);
+    private Vector2 forwardRotationForce = new Vector2(0, 0);
+    private Vector2 backwardRotationForce = new Vector2(0, 0);
+    private float engineAngle = 0; 
+
+    public GameObject engine;
+    public GameObject engineBig;
+    
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        movementForce.x = Input.GetAxis("Horizontal");
+        movementForce.y = Input.GetAxis("Vertical") * 0.8f; 
+        _rigidbody2D.AddForce(movementForce);
+        engineAngle = Mathf.Atan2(0.2f + movementForce.y, movementForce.x) * Mathf.Rad2Deg + 270f;
+        engine.transform.rotation = Quaternion.Euler(0, 0, engineAngle);
+        engineBig.transform.rotation = Quaternion.Euler(0, 0, engineAngle);
+        forwardRotationForce = transform.up * Input.GetAxis("bodyRotation") * 0.05f;
+        backwardRotationForce = transform.up * Input.GetAxis("bodyRotation") * 0.05f;
+        _rigidbody2D.AddForceAtPosition(forwardRotationForce, engineBig.transform.position);
+        _rigidbody2D.AddForceAtPosition(backwardRotationForce, engine.transform.position);
+    }
+}
---
+++ Assets/Scripts/droneToolScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class droneToolScript : MonoBehaviour
+{
+
+    private Vector3 cursorDirection = new Vector2(0, 0);
+
+    public float rotationAngle = 0.1f;
+    public GameObject droneToolFirePoint;
+    void Update()
+    {
+        cursorDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        rotationAngle = Mathf.Atan2(cursorDirection.y, cursorDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotationAngle);
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            RaycastHit2D hit;
+            hit = Physics2D.Raycast(droneToolFirePoint.transform.position, droneToolFirePoint.transform.right);
+            if (hit.collider != null && hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForceAtPosition(droneToolFirePoint.transform.right * 0.4f, hit.point);
+            }
+        }
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            RaycastHit2D hit;
+            hit = Physics2D.Raycast(droneToolFirePoint.transform.position, droneToolFirePoint.transform.right);
+            if (hit.collider != null && hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForceAtPosition(droneToolFirePoint.transform.right * -0.4f, hit.point);
+            }
+        }
+    }
+}
